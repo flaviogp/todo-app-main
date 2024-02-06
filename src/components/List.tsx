059e931c72e19b57485106f124f524a5a051abc0
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-02-06 10:29:29 -0300

    feat: add max height in the list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,5 @@
 
+import { Fragment } from 'react'
 import CrossIcon from '../assets/images/icon-cross.svg'
 import { TodoList } from '../interfaces/interfaces'
 
@@ -48,33 +49,48 @@ const List = ({list, setList}: ListProps) => {
 
 
   return (
-    <ul className='container flex flex-col bg-very-light-gray rounded-md [&>*:not(:first-child)]:border-t-2'>
-    { list.length >= 1 && list.map((todo, index) => (
-            <li 
-                key={`${todo.task}-${index}`}
-                className='flex w-full gap-5 bg-transparent h-[50px] items-center justify-between p-5'
-            >
-                <label htmlFor={`item-${index}`} className='flex gap-5 cursor-pointer w-full'>
-                    <input 
-                        type="checkbox" 
-                        name={`item-${index}`} 
-                        id={`item-${index}`} 
-                        className={`relative appearance-none mr-[10px]
-                            after:w-[20px] after:h-[20px] after:border-2
-                            after:absolute after:top-[2px] 
-                            after:left-0 after:rounded-full ${todo.checked && checkInputStyle}`}
-                        onChange={()=> handleChekTodo(todo.id)}
-                    />
-                    <p className={`${todo.checked ? 'line-through text-light-grayish-blue' : ' text-very-dark-grayish-blue'}`}>{todo.task}</p>
-                </label>
-                <img src={CrossIcon} alt="remove todo" onClick={() => handleDelete(todo.id)} className='w-[17px] h-[17px] flex justify-self-end cursor-pointer'/>
+    <ul className='
+        max-h-[320px]
+        container flex flex-col 
+        bg-very-light-gray rounded-md 
+        '>
+
+        <div className='scroll max-h-[280px] overflow-y-auto 
+        [&>*:not(:first-child)]:border-t-2'>
+            { list.length >= 1 && list.map((todo, index) => (
+                    <li 
+                        key={`${todo.task}-${index}`}
+                        className='flex w-full gap-5 bg-transparent h-[50px] items-center justify-between p-5'
+                    >
+                        <label htmlFor={`item-${index}`} className='flex gap-5 cursor-pointer w-full'>
+                            <input 
+                                type="checkbox" 
+                                name={`item-${index}`} 
+                                id={`item-${index}`} 
+                                className={`relative appearance-none mr-[10px]
+                                    after:w-[20px] after:h-[20px] after:border-2
+                                    after:absolute after:top-[2px] cursor-pointer
+                                    after:left-0 after:rounded-full ${todo.checked && checkInputStyle}`}
+                                onChange={()=> handleChekTodo(todo.id)}
+                            />
+                            <p className={`${todo.checked ? 'line-through text-light-grayish-blue' : ' text-very-dark-grayish-blue'}`}>{todo.task}</p>
+                        </label>
+                        <img src={CrossIcon} alt="remove todo" onClick={() => handleDelete(todo.id)} className='w-[17px] h-[17px] flex justify-self-end cursor-pointer'/>
+                    </li>
+                
+                ))
+            }
+        </div>
+    {  list.length >= 1 ?
+            <li className='flex justify-between p-5 text-dark-grayish-blue'>
+                <p >{`${list.length} items left`}</p>
+                <p className='cursor-pointer' onClick={() => handleClear()}>Clear Completed</p>
             </li>
-        ))
+        :
+            <li className='flex justify-center p-5 text-dark-grayish-blue'>
+                <p>No more Tasks</p>
+            </li>
     }
-    <li className='flex justify-between p-5 text-dark-grayish-blue'>
-        <p >{`${list.length} items left`}</p>
-        <p className='cursor-pointer' onClick={() => handleClear()}>Clear Completed</p>
-    </li>
     </ul>
   )
 }
